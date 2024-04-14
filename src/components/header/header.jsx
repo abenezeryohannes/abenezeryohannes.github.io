3f3acf9c8911c4e77e674d035ac300460ac8e7c0
John Abenezer <abenezeryohannes@gmail.com>
2024-04-14 22:20:08 +0100

    Update header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     return (  
         <header className="header">
             <nav className="nav container">
-                <a href="index.html" className="nav_logo">Abenezer</a>
+                <a href="index.html" className="nav_logo">John</a>
 
                 <div className={Toggle?"nav_menu show-menu":"nav_menu"}>
                     <ul className="nav_list grid">
