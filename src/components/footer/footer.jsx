72cc5b375ceb74ce7de89cc17dedc6e055566136
John Abenezer <abenezeryohannes@gmail.com>
2024-04-14 22:23:21 +0100

    Update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer_container container">
-        <h1 className="footer_title">Abenezer Yohannes</h1>
+        <h1 className="footer_title">John Ebenezer</h1>
         <ul className="footer_list">
           <li>
             <a href="#about" className="footer_link">
